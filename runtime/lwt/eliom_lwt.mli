b82c76489d24e77f40570addb695da7576d6d529
Drup <drupyog@zoho.com>
2016-02-10 02:30:29 +0100

    Reword a bit.

--- runtime/lwt/eliom_lwt.mli
+++ runtime/lwt/eliom_lwt.mli
@@ -1,16 +1,16 @@
 
 (** Implementation of {!Eliom_runtime.Request_data.set_functions}
-    using lwt "thread storage".
+    using lwt's "thread storage".
 
-    This module is aimed at people who want to implement the interface between
-    a given webserver library and eliom. If you simply want to use eliom,
-    this module is not for you.
+    This module is aimed at people who want to implement an interface between
+    a given webserver and eliom. If you simply want to use eliom, this module
+    is not for you.
 *)
 
 val handle_request :
   debug:bool ->
   (unit -> 'a Lwt.t) -> (Eliom_serial.request_data * 'a) Lwt.t
-(** [handle_request ~debug f] calls [f] with a fresh request data.
+(** [handle_request ~debug f] calls [f] with fresh request data.
     When f is done, it returns both the result and the data.
 
     This should be called when starting the handling of a request.
@@ -20,11 +20,11 @@ val handle_request :
 
 val detach_from_request :
   (unit -> 'a) -> 'a
-(** [detach_from_request f] launches f with no request data information.
-    This should only be used to launch an long-running asynchronous
-    functions which is not related to the handling of the current request,
-    in order to avoid memory leaks.
+(** [detach_from_request f] launches [f] with no request data information.
+    This should only be used to avoid memory leaks when launching a
+    long-running asynchronous function which is not related to the handling
+    of the current request.
 
     [f] should not lead to the execution of client fragments
-    (or terrible things will happen, including client process failure).
+    or terrible things will happen, including client process crash.
 *)
